b07cae7872a3284736a98da0d48b92949fdc892b
Mohamed <mohamed.ahmed9833@gmail.com>
2020-03-13 18:37:47 +0700

    backend 2 but not complete

---
+++ WebInt-Backend2-master/Backend2/Models/CalculateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend2.Models
+{
+    public class CalculateViewModel
+    {
+        [Required(ErrorMessage = "First number is required")]
+        public int firstNum { get; set; }
+
+        [Required(ErrorMessage = "Second number is required")]
+        public int secondNum { get; set; }
+
+    }
+}
